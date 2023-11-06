bb82313b5d77900ee237ec93ded1050cda6f94ae
Applebees1 <104962202+Applebees1@users.noreply.github.com>
2023-11-05 18:51:38 -0800

    first

--- client/src/components/Contact/index.jsx
+++ client/src/components/Contact/index.jsx
@@ -33,9 +33,10 @@ const ContactInfo = () => {
         </div>
       ))}
     </div>
-    <a href="https://paypal.me/YouAreSoAwesome?country.x=US&locale.x=en_US" target="_blank">
+   <div><a href="https://paypal.me/YouAreSoAwesome?country.x=US&locale.x=en_US" target="_blank">
   <h1>Buy Me a Coffee 😀:<strong>https://paypal.me/YouAreSoAwesome</strong></h1>
 </a>
+</div> 
     </div>
   );
 };
